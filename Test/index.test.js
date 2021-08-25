7c0a8d27564d9d80adef52f7e9a020a1b695a652
NovandiKevin <kevinpret27@gmail.com>
2021-08-26 00:10:45 +0700

    Data Siswa CRUD dan Testing CD

---
+++ Test/index.test.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+require("mocha");
+describe('First test', () => {
+    it('should return true', () => {
+        const result = true;
+        chai_1.expect(result).to.equal(true);
+    });
+});
