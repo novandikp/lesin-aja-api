f8dcd56b2391fc401eae0f2cbd637f3ce5d97771
KevDoCode <62963736+KevDoCode@users.noreply.github.com>
2021-09-01 14:51:25 +0700

    Penambahan route pembayaran

--- Controller/PembayaranController.ts
+++ Controller/PembayaranController.ts
@@ -5,11 +5,34 @@ import { send } from './../Util/GlobalResponse';
 import { ErrorHandler } from "../Util/ErrorHandler";
 import WaliChecker from "../Middleware/WaliChecker"
 import { addBayar } from "../Service/PembayaranService"
+import path from "path"
 
 const router = Router()
+import multer = require("multer");
 
+const UPLOAD_PATH = 'public/uploads/bukti';
+const upload = multer({ storage:
+    multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, UPLOAD_PATH)
+      },
+
+      filename: function (req, file, cb) {
+        let ext = path.extname(file.originalname)
+        let name= [req.context.iduser,req.context.posisi,new Date().getTime()].join("")
+        let filename =`${name}${ext}`
+        cb(null, filename)
+      },
+    })
+});
+
+router.post("/",upload.any(), async (req:Request,res:Response,next:NextFunction)=>{
+  if (!req.files) return next(new ErrorHandler(HTTPStatus.NOTFOUND,"Harap Upload Bukti pembayaran"))
+  req.body.bukti = req.files[0].filename
+  return next()
+})
 
 router.post("/", async (req:Request,res:Response,next:NextFunction)=>{
 
-
 })
+
