a94b714c690512fe9a44dd2e939d272a50b057b6
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-02-16 19:27:01 +0700

    Penggantian Metode

--- Service/PembayaranService.ts
+++ Service/PembayaranService.ts
@@ -4,9 +4,9 @@ import { LesBuilder } from "../Builder/LesBuilder"
 const addBayar = async (data)  =>{
   try{
     const dataLes = await db.les.get(data.idles)
-    if (dataLes.statusles === StatusLes.MEMILIH_GURU) {
+    if (dataLes.statusles === StatusLes.PENDING) {
       const lesBuilder: LesBuilder = new LesBuilder(dataLes)
-      lesBuilder.setPending()
+      lesBuilder.setBerlangsung()
       db.les.edit(lesBuilder.build(), data.idles)
       return await db.pembayaran.add(data)
     }
