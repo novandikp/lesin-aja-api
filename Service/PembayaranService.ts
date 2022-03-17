7b5fe15c796318b0eee08fe789587746baddb438
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-03-17 12:11:45 +0700

    bayar ulang

--- Service/PembayaranService.ts
+++ Service/PembayaranService.ts
@@ -4,7 +4,7 @@ import { LesBuilder } from "../Builder/LesBuilder"
 const addBayar = async (data)  =>{
   try{
     const dataLes = await db.les.get(data.idles)
-    if (dataLes.statusles === StatusLes.PENDING) {
+    if (dataLes.statusles === StatusLes.PENDING || dataLes.statusles === StatusLes.PEMBAYARANDITOLAK) {
       const lesBuilder: LesBuilder = new LesBuilder(dataLes)
       lesBuilder.setBayarBelumKonfirmasi()
       db.les.edit(lesBuilder.build(), data.idles)
